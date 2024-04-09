0e9cdcc2d16d2ed05fe74467f5112afeba0f3354
Udayabharathim <160568654+Udayabharathim@users.noreply.github.com>
2024-04-09 21:12:52 +0530

    Update Exp3_Multiplier_2bit.v

--- Exp3_Multiplier_2bit.v
+++ Exp3_Multiplier_2bit.v
@@ -1,12 +1,14 @@
-module ha(a,b,sum,carry);
-input a,b;
-output sum,carry;
-
-endmodule
-
-module multi_2(a,b,p,carry);
-input [1:0]a,b;
-output [2:0]p;
-output carry;
-
-endmodule
+module ha(a,b,s,carry); 
+input a,b; 
+output s,carry; 
+assign s=a^b; 
+assign carry=a&b; 
+endmodule 
+ module multiplier_2(a,b,c); 
+input [1:0]a,b; 
+output [3:0]c; 
+wire w; 
+assign c[0]=a[0]&b[0]; 
+ha h1(a[0]&b[1],a[1]&b[0],c[1],w); 
+ha h2(a[1]&b[1],w,c[2],c[3]); 
+endmodule 
